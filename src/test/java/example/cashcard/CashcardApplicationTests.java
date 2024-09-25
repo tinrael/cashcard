93df176297608fb23550ede6913c5e6696646773
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-09-25 21:24:11 +0300

    Add a Spring Boot Test for the GET endpoint

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -1,11 +1,26 @@
 package example.cashcard;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardApplicationTests {
-
+	@Autowired
+	TestRestTemplate restTemplate;
+	
+	@Test
+	void shouldReturnACashCardWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+		
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+	
 	@Test
 	void contextLoads() {
 	}

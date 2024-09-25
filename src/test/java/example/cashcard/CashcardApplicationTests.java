b61d24c3f13d68c011ae3d2753ce6a310e234198
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-09-25 21:50:40 +0300

    Add a new test method

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -33,6 +33,14 @@ class CashcardApplicationTests {
 	}
 	
 	@Test
+	void shouldNotReturnACashCardWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+		
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
+	
+	@Test
 	void contextLoads() {
 	}
 

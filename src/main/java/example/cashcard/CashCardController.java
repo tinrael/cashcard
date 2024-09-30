f6149fb799aafe96141fe0520725e4149d611afd
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-09-30 18:02:53 +0300

    Inject the CashCardRepository into CashCardController

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+	private final CashCardRepository cashCardRepository;
+	
+	private CashCardController(CashCardRepository cashCardRepository) {
+		this.cashCardRepository = cashCardRepository;
+	}
+	
 	@GetMapping("/{requestedId}")
 	private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
 		if (requestedId.equals(99L)) {

c0c589b322356853c9b98f1f8ffd3c3bbfe1b339
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-09-30 18:19:33 +0300

    Update the CashCardController to utilize findById method on the CashCardRepository and update the logic

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,5 +1,7 @@
 package example.cashcard;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +19,10 @@ public class CashCardController {
 	
 	@GetMapping("/{requestedId}")
 	private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-		if (requestedId.equals(99L)) {
-			CashCard cashCard = new CashCard(99L, 123.45);
-			return ResponseEntity.ok(cashCard);
+		Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+		
+		if (cashCardOptional.isPresent()) {
+			return ResponseEntity.ok(cashCardOptional.get());
 		} else {
 			return ResponseEntity.notFound().build();
 		}

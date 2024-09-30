3f4ead7112253038f2364161002c7dc23a8f70ed
Tymur Krasnopir <krasnopir.std@gmail.com>
2024-09-30 17:52:38 +0300

    Configure the CashCardRepository

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -2,6 +2,6 @@ package example.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
 
-interface CashCardRepository extends CrudRepository {
+interface CashCardRepository extends CrudRepository<CashCard, Long> {
 
 }
